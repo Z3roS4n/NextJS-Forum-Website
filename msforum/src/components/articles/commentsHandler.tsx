8fc5d2871861ff5aee350ba4333f9642640ea337
Antonio Murabito <Thepro007002001@gmail.com>
2025-07-11 17:07:34 +0200

    Improve comment handling and navbar sign-in link
    Updated commentsHandler to support async user checks, display a comment form for logged-in users, and visually distinguish user comments. Fixed navbar to link to '/sign-in' instead of '/sign-up' and removed loading state for improved UX.

--- msforum/src/components/articles/commentsHandler.tsx
+++ msforum/src/components/articles/commentsHandler.tsx
@@ -1,17 +1,38 @@
 import { Comment_Author_Subscription } from "@/types/components";
+import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
 
 interface Params {
     comments: Comment_Author_Subscription[];
 }
 
-const CommentHandler = ( { comments }: Params ) => {
+const CommentHandler = async ( { comments }: Params ) => {
+    const user = await currentUser();
+
+    const checkLogged = (author_id: string) => {
+        if(author_id == user?.id) 
+            return true;
+        return false;
+    }
+
     return (
         <div className="flex flex-col gap-2">
+
+            {
+                user?.id ? //load this component only if user is logged in.
+                    <div className="border-2 rounded-2xl p-4 w-1/1 flex flex-col gap-2">
+                        <label className="font-bold text-lg" htmlFor="leaveComment">Leave a Comment</label>
+                        <textarea className="border-1 rounded-xl p-2" id="leaveComment" rows={4}/>
+                        <button className="btn-primary lg:w-1/4">Submit Comment</button>
+                    </div>
+                    : ""
+            }
+
             {comments.map((comment, index) =>
                 // Check if comment is written by logged user or not,
                 // if logged: "Username" has to be "You" and have another border/shadown color
-                <div className="border-2 rounded-2xl p-4 w-1/2"  key={index}>
-                    <h3>{comment.author.user_id} - {comment.author?.subscription?.name ?? "Starter User"}</h3>
+                <div className={"border-2 rounded-2xl p-4 lg:w-1/2 overflow-hidden" + (!checkLogged(comment.author.user_id) ? "self-start" : "self-end") } key={index}>
+                    <Link href={`/profile/${comment.user_id}`} className="font-bold text-lg text-ellipsis">{!checkLogged(comment.author.user_id) ? comment.author.user_id : "You"} - {comment.author?.subscription?.name ?? "Starter User"}</Link>
                     <p>{comment.datetime}</p>
                     <p>{comment.content}</p>
                 </div>
--- msforum/src/components/navbar.tsx
+++ msforum/src/components/navbar.tsx
@@ -7,7 +7,6 @@ import { SignedIn, SignedOut, useUser, UserButton } from "@clerk/nextjs";
 const NavBar = () => {
     const { isLoaded, isSignedIn, user} = useUser();
 
-    if(!isLoaded) return <p>Loading...</p>
     return (
         <nav className="p-6 flex flex-row justify-between">
             <div>
@@ -29,7 +28,7 @@ const NavBar = () => {
                     <UserButton userProfileUrl="/profile" showName></UserButton>
                 </SignedIn>
                 <SignedOut>
-                    <Link className="font-medium" href='/sign-up'>Sign In</Link>
+                    <Link className="font-medium" href='/sign-in'>Sign In</Link>
                 </SignedOut>
             </div>
         </nav>
